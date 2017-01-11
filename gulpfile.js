d953e6617fe50d0c1be9f33a936fa7da602ff5ff
Alexandre Kieling <alex.kieling@gmail.com>
2017-01-11 16:38:31 -0200

    rollback index redirect and /console/java context

--- gulpfile.js
+++ gulpfile.js
@@ -224,7 +224,7 @@ gulp.task('watch', ['build', 'build-example', 'watch-less'], function() {
 
 function configStaticAssets(prefix) {
   var staticAssets = [{
-      path: '/console/java',
+      path: '/',
       dir: prefix
   }];
   var targetDir = urljoin(prefix, 'libs');
@@ -237,7 +237,7 @@ function configStaticAssets(prefix) {
         if (fs.statSync(dir).isDirectory()) {
           console.log("Adding directory to search path: ", dir);
           staticAssets.push({
-            path: '/console/java',
+            path: '/',
             dir: dir
           });
         }
