65674fd522409d237b804a99b67c7cf1820130a1
Alexandre Kieling <abrianik@redhat.com>
2017-09-17 21:24:38 -0300

    fix openshift-3.3 branch build

--- maybeDeploy.sh
+++ maybeDeploy.sh
@@ -3,27 +3,12 @@
 git config --global push.default matching
 git fetch --tags
 
-#LATEST=`cat LATEST`
-#CURRENT=`git tag --list | grep -v build | sort --version-sort | tail -n 1`
-
-#echo "Latest on disk: $LATEST"
-#echo "Latest in repo: $CURRENT"
-
-#if [ "$CURRENT" != "$LATEST" ]
-#then
-  echo "Deploying new build"
-  git config --global user.email "circleci@mail.com" && \
-  git config --global user.name "circleci" && \
-  echo "Cleaning and rebuilding" && \
-  rm -Rf site/* && \
-  gulp bower path-adjust && \
-  gulp build && \
-  gulp site && \
-  gulp deploy && \
-  echo $CURRENT > LATEST && \
-  git add LATEST && \
-  git commit -m "[ci skip] Updating latest tag" && \
-  git pull && git push && git push --tags
-#else
-#  echo "Not deploying new build"
-#fi
+echo "Deploying new build"
+git config --global user.email "circleci@mail.com" && \
+git config --global user.name "circleci" && \
+echo "Cleaning and rebuilding" && \
+rm -Rf site/* && \
+gulp bower path-adjust && \
+gulp build && \
+gulp site && \
+gulp deploy
